f9c923cc1c920c871774e2ab24efa46aedae3f10
sbx_user1051 <sbx_user1051@169.254.236.53>
2019-11-03 12:28:52 +0000

    Sun Nov 03 12:28:52 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,11 +1,14 @@
 # noinspection PyUnusedLocal
 def fizz_buzz(number):
     strings = []
-    if number % 3 == 0:
+    # fizz if divisible by 3 or has a 3 in it
+    if (number % 3 == 0) or ("3" in str(number)):
         strings.append("fizz")
-    if number % 5 == 0:
+    # buzz if divisible by 5 or it has a 5 in it:
+    if (number % 5 == 0) or ("5" in str(number)):
         strings.append("buzz")
     if len(strings) > 0:
         return(" ".join(strings))
     else:
-        return(number)
+        return(str(number))
+
