63188bf4f3bb8e73c8c73671a508579a991a8af8
sbx_user1051 <sbx_user1051@169.254.236.53>
2019-11-03 12:57:07 +0000

    Sun Nov 03 12:57:07 UTC 2019

--- test/solution_tests/FIZ/test_fizzbuzz.py
+++ test/solution_tests/FIZ/test_fizzbuzz.py
@@ -3,32 +3,29 @@ from solutions.FIZ import fizz_buzz_solution
 
 class TestSum():
     def test_fizz_div_by_3(self):
-        assert fizz_buzz_solution.fizz_buzz(3) == "fizz"
+        assert fizz_buzz_solution.fizz_buzz(6) == "fizz"
     def test_fizz_includes_3(self):
         assert fizz_buzz_solution.fizz_buzz(13) == "fizz"
     def test_buzz_div_by_5(self):
-        assert fizz_buzz_solution.fizz_buzz(5) == "buzz"
+        assert fizz_buzz_solution.fizz_buzz(10) == "buzz"
     def test_buzz_include_5(self):
         assert fizz_buzz_solution.fizz_buzz(502) == "buzz"
     def test_fizz_buzz_div_by_3_and_5(self):
-        assert fizz_buzz_solution.fizz_buzz(15) == "fizz buzz"
+        assert fizz_buzz_solution.fizz_buzz(60) == "fizz buzz"
     def test_fizz_buzz_div_by_3_contains_5(self):
         assert fizz_buzz_solution.fizz_buzz(501) == "fizz buzz"
     def test_fizz_buzz_contains_3_div_by_5(self):
-        assert fizz_buzz_solution.fizz_buzz(35) == "fizz buzz"
+        assert fizz_buzz_solution.fizz_buzz(6310) == "fizz buzz"
     def test_fizz_buzz_contains_3_and_5(self):
         assert fizz_buzz_solution.fizz_buzz(503) == "fizz buzz"
-    def test_fake_deluxe(self):
-        assert fizz_buzz_solution.fizz_buzz(11) == "fake deluxe"
-    def test_deluxe(self):
-        assert fizz_buzz_solution.fizz_buzz(22) == "deluxe"
-    def text_deluxe_div_by_3(self):
-        assert fizz_buzz_solution.fizz_buzz(66) == "fizz deluxe"
-    def text_fake_deluxe_div_by_3(self):
-        assert fizz_buzz_solution.fizz_buzz(33) == "fizz fake deluxe"
-    def text_fake_deluxe_div_by_5(self):
-        assert fizz_buzz_solution.fizz_buzz(55) == "buzz fake deluxe"
-    def text_fizz_buzz_deluxe(self):
-        assert fizz_buzz_solution.fizz_buzz(555) == "fizz buzz fake deluxe"
+    def test_deluxe_contains_3_and_div_by_3(self):
+        assert fizz_buzz_solution.fizz_buzz(36) == "fizz deluxe"
+    def test_deluxe_contains_5_and_div_by_5(self):
+        assert fizz_buzz_solution.fizz_buzz(50) == "buzz deluxe"
+    def test_fake_deluxe_contains_3_and_div_by_3(self):
+        assert fizz_buzz_solution.fizz_buzz(3) == "fizz fake deluxe"
+    def test_fake_deluxe_contains_5_and_div_by_5(self):
+        assert fizz_buzz_solution.fizz_buzz(5) == "buzz fake deluxe"
     def test_neither(self):
         assert fizz_buzz_solution.fizz_buzz(7) == "7"
+
