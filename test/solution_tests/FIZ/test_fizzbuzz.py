a68d3ed60b8468b8ec3b1951f967c758b163524f
sbx_user1051 <sbx_user1051@169.254.236.53>
2019-11-03 12:30:32 +0000

    Sun Nov 03 12:30:32 UTC 2019

--- test/solution_tests/FIZ/test_fizzbuzz.py
+++ test/solution_tests/FIZ/test_fizzbuzz.py
@@ -2,11 +2,22 @@ from solutions.FIZ import fizz_buzz_solution
 
 
 class TestSum():
-    def test_fizz(self):
+    def test_fizz_div_by_3(self):
         assert fizz_buzz_solution.fizz_buzz(3) == "fizz"
-    def test_buzz(self):
+    def test_fizz_includes_3(self):
+        assert fizz_buzz_solution.fizz_buzz(13) == "fizz"
+    def test_buzz_div_by_5(self):
         assert fizz_buzz_solution.fizz_buzz(5) == "buzz"
-    def test_fizz_buzz(self):
+    def test_buzz_include_5(self):
+        assert fizz_buzz_solution.fizz_buzz(502) == "buzz"
+    def test_fizz_buzz_div_by_3_and_5(self):
         assert fizz_buzz_solution.fizz_buzz(15) == "fizz buzz"
+    def test_fizz_buzz_div_by_3_contains_5(self):
+        assert fizz_buzz_solution.fizz_buzz(501) == "fizz buzz"
+    def test_fizz_buzz_contains_3_div_by_5(self):
+        assert fizz_buzz_solution.fizz_buzz(35) == "fizz buzz"
+    def test_fizz_buzz_contains_3_and_5(self):
+        assert fizz_buzz_solution.fizz_buzz(503) == "fizz buzz"
     def test_neither(self):
-        assert fizz_buzz_solution.fizz_buzz(7) == 7
+        assert fizz_buzz_solution.fizz_buzz(7) == "7"
+
