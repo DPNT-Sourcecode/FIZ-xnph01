2ac77c19776aea9ccd3d622a90dfdc70d8259f77
sbx_user1051 <sbx_user1051@169.254.236.53>
2019-11-03 11:59:11 +0000

    Sun Nov 03 11:59:11 UTC 2019

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,4 +3,8 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
-    raise NotImplementedError()
+    if isinstance(obj_to_test, str):
+        return(f"Hello {friend_name}")
+    else:
+        return("Please enter a string")
+
